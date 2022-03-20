e1ed601fe05899565deb8367c1951d6a57419220
Laith Harb <laithharb@Laiths-iMac.local>
2022-03-20 16:21:46 -0400

    feature: add option state

--- 01-Baby-Name-Generator/app.vue
+++ 01-Baby-Name-Generator/app.vue
@@ -1,3 +1,11 @@
+<script setup>
+const options = reactive({
+  gender: "Girl",
+  popularity: "Unique",
+  length: "Long",
+});
+</script>
+
 <template>
   <div class="container">
     <h1>Baby Name Generator</h1>
@@ -6,24 +14,64 @@
       <div class="option-container">
         <h4>1) Choose a gender</h4>
         <div class="option-buttons">
-          <button class="option option-left">Boy</button>
-          <button class="option">Unisex</button>
-          <button class="option option-right">Girl</button>
+          <button
+            class="option option-left"
+            :class="options.gender === 'Boy' && 'option-active'"
+          >
+            Boy
+          </button>
+          <button
+            class="option"
+            :class="options.gender === 'Unisex' && 'option-active'"
+          >
+            Unisex
+          </button>
+          <button
+            class="option option-right"
+            :class="options.gender === 'Girl' && 'option-active'"
+          >
+            Girl
+          </button>
         </div>
       </div>
       <div class="option-container">
         <h4>2) Choose the name's popularity</h4>
         <div class="option-buttons">
-          <button class="option option-left">Trendy</button>
-          <button class="option option-right">Unique</button>
+          <button
+            class="option option-left"
+            :class="options.popularity === 'Trendy' && 'option-active'"
+          >
+            Trendy
+          </button>
+          <button
+            class="option option-right"
+            :class="options.popularity === 'Unique' && 'option-active'"
+          >
+            Unique
+          </button>
         </div>
       </div>
       <div class="option-container">
         <h4>2) Choose name's length</h4>
         <div class="option-buttons">
-          <button class="option option-left">Long</button>
-          <button class="option">All</button>
-          <button class="option option-right">Short</button>
+          <button
+            class="option option-left"
+            :class="options.length === 'Long' && 'option-active'"
+          >
+            Long
+          </button>
+          <button
+            class="option"
+            :class="options.length === 'All' && 'option-active'"
+          >
+            All
+          </button>
+          <button
+            class="option option-right"
+            :class="options.length === 'Short' && 'option-active'"
+          >
+            Short
+          </button>
         </div>
       </div>
     </div>
@@ -75,5 +123,10 @@ h1 {
 
 .option-right {
   border-radius: 0 1rem 1rem 0;
+}
+
+.option-active {
+  background-color: rgb(249, 87, 89);
+  color: white;
 }
 </style>
