fa3e692b6f32046169ad3ddc6958362b718f98cf
Laith Harb <laithharb@Laiths-iMac.local>
2022-03-20 15:42:25 -0400

    refactor: add base html template

--- 01-Baby-Name-Generator/app.vue
+++ 01-Baby-Name-Generator/app.vue
@@ -1,3 +1,31 @@
 <template>
-  <div></div>
+  <div>
+    <h1>Baby Name Generator</h1>
+    <p>Choose your options and click the "Find Names" buttom below</p>
+    <div class="options-container">
+      <div class="option-container">
+        <h4>1) Choose a gender</h4>
+        <div class="option-buttons">
+          <button>Boy</button>
+          <button>Unisex</button>
+          <button>Girl</button>
+        </div>
+      </div>
+      <div class="option-container">
+        <h4>2) Choose the name's popularity</h4>
+        <div class="option-buttons">
+          <button>Trendy</button>
+          <button>Unique</button>
+        </div>
+      </div>
+      <div class="option-container">
+        <h4>2) Choose name's length</h4>
+        <div class="option-buttons">
+          <button>Long</button>
+          <button>All</button>
+          <button>Short</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
