6dd4d708fead53a21034ec96937ad6d7cd36591b
Laith Harb <laithharb@Laiths-iMac.local>
2022-03-20 19:43:26 -0400

    feature: add name card

--- 01-Baby-Name-Generator/app.vue
+++ 01-Baby-Name-Generator/app.vue
@@ -106,7 +106,12 @@ const selectedNames = ref<string[]>([]);
       </div>
       <button class="primary" @click="computeSelectedNames">Find Names</button>
     </div>
-    {{ selectedNames }}
+    <div class="cards-container">
+      <div v-for="name in selectedNames" :key="name" class="card">
+        <h4>{{ name }}</h4>
+        <p>x</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -171,5 +176,30 @@ h1 {
   font-size: 1rem;
   margin-top: 1rem;
   cursor: pointer;
+}
+
+.cards-container {
+  display: flex;
+  margin-top: 3rem;
+  flex-wrap: wrap;
+}
+
+.card {
+  background-color: rgb(27, 60, 138);
+  width: 28%;
+  color: white;
+  border-radius: 1rem;
+  padding: 0.1rem;
+  margin-right: 0.5rem;
+  margin-bottom: 1rem;
+  position: relative;
+}
+
+.card p {
+  position: absolute;
+  top: -29%;
+  left: 92.5%;
+  cursor: pointer;
+  color: rgba(255, 255, 255, 0.178);
 }
 </style>
