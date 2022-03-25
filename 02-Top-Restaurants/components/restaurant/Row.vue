c0c905de715cc19647a6548b9fd3d0ca8df4728e
Laith Harb <laithharb@Laiths-iMac.local>
2022-03-25 19:03:48 -0400

    add boilerplate code

---
+++ 02-Top-Restaurants/components/restaurant/Row.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="row">
+    <h4 class="header rank">1</h4>
+    <a href="/" class="header link">McDonalds</a>
+  </div>
+</template>
+
+<style scoped>
+.row {
+  border-top: 1px solid rgba(128, 128, 128, 0.336);
+  display: flex;
+  align-items: center;
+}
+
+.header {
+  font-size: 1.25rem;
+  color: rgb(10, 31, 148);
+  margin: 0;
+  margin-right: 2rem;
+  padding: 0.5rem 1rem;
+  font-weight: 700;
+  width: 4rem;
+}
+.rank {
+  color: black;
+}
+.link {
+  text-decoration: none;
+  color: rgb(228, 157, 27);
+  width: auto;
+  font-weight: 500;
+}
+</style>
---
+++ 02-Top-Restaurants/components/restaurant/Table.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="table">
+    <h1>TOP 50: THE RANKING</h1>
+    <div class="table-container">
+      <div class="table-col"></div>
+      <div class="table-col"></div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.table {
+  margin: 3rem 0;
+}
+.table h1 {
+  margin-bottom: 2rem;
+}
+.table-container {
+  display: flex;
+  justify-content: space-between;
+}
+.table-col {
+  width: 48%;
+}
+</style>
