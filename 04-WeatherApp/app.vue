f893feeb2dbc159c628be6dc6b118fcd8b15a804
Laith Harb <laithharb@Laiths-iMac.local>
2022-04-10 11:46:43 -0400

    define public runtime

--- 04-WeatherApp/app.vue
+++ 04-WeatherApp/app.vue
@@ -1,9 +1,54 @@
 <script setup lang="ts">
-const search = ref("Toronto");
-const input = ref("");
+const cookie = useCookie("city");
+const config = useRuntimeConfig();
 
-const { data: city, error } = useFetch(
-  `https://api.openweathermap.org/data/2.5/weather?q=${search.value}&units=metric&appid=64e28f64b16b0f6cfeea0039ad3536da`
+console.log({ config });
+if (!cookie.value) cookie.value = "Toronto";
+
+const search = ref(cookie.value);
+const input = ref("");
+const background = ref("");
+
+const { data: city, error } = useAsyncData(
+  "city",
+  async () => {
+    let response;
+
+    try {
+      response = await $fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${search.value}`,
+        {
+          params: {
+            units: "metric",
+            appid: config.WEATHER_APP_SECRET,
+          },
+        }
+      );
+
+      cookie.value = search.value;
+
+      const temp = response.main.temp;
+
+      if (temp <= -10) {
+        background.value =
+          "https://images.unsplash.com/photo-1483664852095-d6cc6870702d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3540&q=80";
+      } else if (temp > -10 && temp <= 0) {
+        background.value =
+          "https://images.unsplash.com/photo-1476820865390-c52aeebb9891?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3540&q=80";
+      } else if (temp > 0 && temp <= 10) {
+        background.value =
+          "https://images.unsplash.com/photo-1560258018-c7db7645254e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=4032&q=80";
+      } else {
+        background.value =
+          "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3546&q=80";
+      }
+    } catch (e) {}
+
+    return response;
+  },
+  {
+    watch: [search],
+  }
 );
 
 const handleClick = () => {
@@ -15,8 +60,7 @@ const handleClick = () => {
 
 <template>
   <div class="h-screen relative overflow-hidden">
-    <img />
-    {{ search }}
+    <img :src="background" />
     <div class="absolute w-full h-full top-0 overlay" />
     <div class="absolute w-full h-full top-0 p-48">
       <div class="flex justify-between">
